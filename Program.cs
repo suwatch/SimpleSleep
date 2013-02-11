11f9dfa9f23c3f1b1fb64d74f787c978f05aacf9
Suwat Ch <suwatch@microsoft.com>
2013-02-11 13:57:02 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SimpleSleep
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                foreach (string arg in args)
+                {
+                    int timeout = Int32.Parse(arg);
+                    Thread.Sleep(timeout);
+                    Console.WriteLine("Sleep(" + timeout + ")");
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return -1;
+            }
+        }
+    }
+}
